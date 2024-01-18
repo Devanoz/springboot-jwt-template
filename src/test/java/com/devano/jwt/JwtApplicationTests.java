c1332fe2d1dd25fbf54f4d1609d26d9a1037e703
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-01-18 20:43:30 +0700

    first commit

---
+++ src/test/java/com/devano/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devano.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
