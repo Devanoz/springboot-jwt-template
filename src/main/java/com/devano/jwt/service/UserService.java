3955b9478fea98d2bb86dfbe3d62b36e96957403
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-19 20:42:40 +0700

    edit return type

--- src/main/java/com/devano/jwt/service/UserService.java
+++ src/main/java/com/devano/jwt/service/UserService.java
@@ -1,12 +1,11 @@
 package com.devano.jwt.service;
 
 
-import com.devano.jwt.dto.GlobalResponse;
-import com.devano.jwt.dto.auth.login.LoginRequestDto;
 import com.devano.jwt.dto.auth.register.RegisterRequestDto;
+import com.devano.jwt.dto.auth.register.RegisterResponseDto;
 
 public interface UserService {
 
-    GlobalResponse<Object> register(RegisterRequestDto registerRequestDto);
+    RegisterResponseDto register(RegisterRequestDto registerRequestDto);
 
 }
