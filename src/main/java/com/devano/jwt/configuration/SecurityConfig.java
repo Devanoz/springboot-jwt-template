1447cdc173644cd072016f4565239dc13747c6a2
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-19 20:41:59 +0700

    add Security Config update: - bean AuthenticationManager - edit Access denied hanlder to provide specific messages - register access denied handler

--- src/main/java/com/devano/jwt/configuration/SecurityConfig.java
+++ src/main/java/com/devano/jwt/configuration/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.devano.jwt.configuration;
 
-import com.devano.jwt.auth.JwtAuthorizationFilter;
+import com.devano.jwt.auth.JwtAuthenticationFilter;
 import com.devano.jwt.service.impl.CustomUserDetailService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
@@ -8,12 +8,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -26,33 +29,26 @@ import java.io.PrintWriter;
 public class SecurityConfig {
 
     private final CustomUserDetailService userDetailsService;
-    private final JwtAuthorizationFilter jwtAuthorizationFilter;
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
-            throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-        return authenticationManagerBuilder.build();
-    }
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .anonymous(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(matcherRegistry -> {
-                    matcherRegistry.requestMatchers("/api/v1/user/**").hasAuthority("USER");
+                    matcherRegistry.requestMatchers("/api/v1/user/**").hasAnyAuthority("USER","ADMIN");
                     matcherRegistry.requestMatchers("/api/v1/admin/**").hasAuthority("ADMIN");
                     matcherRegistry.requestMatchers("/api/v1/auth/**").permitAll();
                     matcherRegistry.anyRequest().authenticated();
                 })
+                .authenticationManager(authenticationManager())
+                .exceptionHandling(handler -> {
+                    handler.accessDeniedHandler(accessDeniedHandler());
+                })
                 .sessionManagement(sessionManagementConfigurer -> {
                     sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 });
-        http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -64,14 +60,26 @@ public class SecurityConfig {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.setContentType("application/json");
             try (PrintWriter writer = response.getWriter()) {
-                writer.write("{\"error\": \""+ exception.getMessage() +"\"}");
+                writer.write("{\"error\": \""+ "role is not permitted"  +"\"}");
             }
         };
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return new ProviderManager(authenticationProvider());
+    }
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
