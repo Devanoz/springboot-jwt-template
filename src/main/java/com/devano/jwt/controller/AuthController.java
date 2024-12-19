46ff101f941c267a4c8b25b5ec7dbbb35dc28a15
Muhamamad Devano Zaidan <80044118+Devanoz@users.noreply.github.com>
2024-12-19 20:39:18 +0700

    add authentication service for auth controller

--- src/main/java/com/devano/jwt/controller/AuthController.java
+++ src/main/java/com/devano/jwt/controller/AuthController.java
@@ -1,66 +1,37 @@
 package com.devano.jwt.controller;
 
-import com.devano.jwt.auth.JwtUtil;
-import com.devano.jwt.dto.GlobalResponse;
+import com.devano.jwt.dto.auth.AuthenticationResponse;
 import com.devano.jwt.dto.auth.login.LoginRequestDto;
-import com.devano.jwt.dto.auth.login.LoginResponseDto;
 import com.devano.jwt.dto.auth.register.RegisterRequestDto;
-import com.devano.jwt.exception.UserNotFoundException;
-import com.devano.jwt.model.User;
-import com.devano.jwt.repository.UserRepository;
+import com.devano.jwt.dto.auth.register.RegisterResponseDto;
+import com.devano.jwt.service.AuthenticationService;
 import com.devano.jwt.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @AllArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtUtil jwtUtil;
-
-    private final UserRepository userRepository;
-
+    private final AuthenticationService authenticationService;
     private final UserService userService;
 
+
     @PostMapping("/login")
-    public ResponseEntity<GlobalResponse<Object>> login(@RequestBody LoginRequestDto loginRequest) {
-        try {
-            Optional<User> userOptionalByEmail = userRepository.findByEmail(loginRequest.getEmail());
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-            User user;
-            if(userOptionalByEmail.isPresent()){
-                user = userOptionalByEmail.get();
-            }else{
-                throw new UserNotFoundException("email tidak ditemukan");
-            }
-            String token = jwtUtil.createToken(user);
-
-            LoginResponseDto loginResponseDto = new LoginResponseDto();
-            loginResponseDto.setEmail(user.getEmail());
-            loginResponseDto.setToken(token);
-
-            return ResponseEntity.ok(GlobalResponse.success("Login berhasil",loginResponseDto));
-
-        }catch (BadCredentialsException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GlobalResponse.error("invalid username or password"));
-        }catch (Exception e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GlobalResponse.error(e.getMessage()));
-        }
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequestDto loginRequest) {
+        String jwtToken = authenticationService.authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+        return ResponseEntity.ok(AuthenticationResponse.builder().token(jwtToken).build());
     }
 
     @PostMapping("/register")
-    public ResponseEntity<GlobalResponse<Object>> register(@RequestBody RegisterRequestDto registerRequestDto){
-        GlobalResponse<Object> registerResponse = userService.register(registerRequestDto);
-        return ResponseEntity.ok(registerResponse);
+    public ResponseEntity<RegisterResponseDto> register(@RequestBody RegisterRequestDto registerRequestDto){
+        RegisterResponseDto response = userService.register(registerRequestDto);
+        return ResponseEntity.ok(response);
     }
 
 }
